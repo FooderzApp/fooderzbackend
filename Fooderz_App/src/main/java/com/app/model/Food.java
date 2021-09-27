eb411a40b3d070f29a08087f2241f338dceffd63
ikkurthi manikumar <you@example.com>
2021-09-27 12:34:32 +0530

    added_image

--- Fooderz_App/src/main/java/com/app/model/Food.java
+++ Fooderz_App/src/main/java/com/app/model/Food.java
@@ -1,10 +1,12 @@
 package com.app.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
@@ -21,6 +23,9 @@ public class Food {
 	private int foodId;
 	private String foodName;
 	private double price;
+	@Lob
+	@Column(columnDefinition = "CLOB")
+	private String avatar;
 	
 	@JsonIgnore
 	@ManyToOne(fetch = FetchType.LAZY)
