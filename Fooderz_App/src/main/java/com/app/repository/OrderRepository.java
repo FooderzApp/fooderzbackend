28dd3b06466d67219af39afa262e835744e769b6
unknown <hrj7292@gmial.com>
2021-10-02 13:13:13 +0530

    Order Part Done

---
+++ Fooderz_App/src/main/java/com/app/repository/OrderRepository.java
@@ -0,0 +1,19 @@
+package com.app.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.app.model.Customer;
+import com.app.model.Order;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+	public List<Order> findOrderByCustomer(Customer customer);
+
+	public List<Order> findOrderByOrderStatus(String orderStatus);
+
+	// public List<Order> findOrderByFood(Food food);
+
+}
